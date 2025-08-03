cb80b64bcca3c9d396dc448ad63afe5148ff920a
wjdw <wpiasecka@gmail.com>
2025-08-03 22:06:03 +0200

    debug output vars

--- scripts/create_or_update_db_user.ps1
+++ scripts/create_or_update_db_user.ps1
@@ -2,27 +2,33 @@ $ErrorActionPreference = "Stop"
 
 Write-Host "Creating or updating database user..."
 
-az login --service-principal `
-  --username $env:AZURE_CLIENT_ID `
-  --password $env:AZURE_CLIENT_SECRET `
-  --tenant $env:AZURE_TENANT_ID `
-  --allow-no-subscriptions
+try {
+  az login --service-principal `
+    --username $env:AZURE_CLIENT_ID `
+    --password $env:AZURE_CLIENT_SECRET `
+    --tenant $env:AZURE_TENANT_ID `
+    --allow-no-subscriptions
 
-az account set --subscription $env:AZURE_SUBSCRIPTION_ID
+  az account set --subscription $env:AZURE_SUBSCRIPTION_ID
 
-write-host "fqdn: $($env:SQL_FQDN)"
-write-host "db: $($env:SQL_DB)"
-write-host "app principal id: $($env:APP_PRINCIPAL_ID)"
+  write-host "fqdn: $($env:SQL_FQDN)"
+  write-host "db: $($env:SQL_DB)"
+  write-host "app principal id: $($env:APP_PRINCIPAL_ID)"
 
-$sqlCommand = @"
+  $sqlCommand = @"
 DROP USER IF EXISTS [$($env:APP_PRINCIPAL_ID)];
 CREATE USER [$($env:APP_PRINCIPAL_ID)] FROM EXTERNAL PROVIDER;
 ALTER ROLE db_datareader ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
 ALTER ROLE db_datawriter ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
 "@
 
-sqlcmd -S $env:SQL_FQDN `
-  -d $env:SQL_DB `
-  -U $env:SQL_ADMIN_LOGIN `
-  -P $env:SQL_ADMIN_PASSWORD `
-  -Q $sqlCommand
+  sqlcmd -S $env:SQL_FQDN `
+    -d $env:SQL_DB `
+    -U $env:SQL_ADMIN_LOGIN `
+    -P $env:SQL_ADMIN_PASSWORD `
+    -Q $sqlCommand
+
+}
+catch {
+  Throw "An error occurred while creating or updating the database"
+}
