fd328189099a30061c773d246f4bf6c10338b157
wjdw <wpiasecka@gmail.com>
2025-08-03 19:55:09 +0200

    replace apt-key with gpg and reference keyring

--- scripts/install_sqlcmd.sh
+++ scripts/install_sqlcmd.sh
@@ -1,7 +1,9 @@
 set -e
 
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl -sSL https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+curl -sSL https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/microsoft-prod.list
+
+sudo sed -i 's|deb |deb [signed-by=/usr/share/keyrings/microsoft-prod.gpg] |' /etc/apt/sources.list.d/microsoft-prod.list
 
 sudo apt-get update
 
