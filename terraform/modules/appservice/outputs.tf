54008f43be431a8fe8d1bffc6d8e8159b3d3bd4f
wjdw <wpiasecka@gmail.com>
2025-08-03 17:21:48 +0200

    correct output

--- terraform/modules/appservice/outputs.tf
+++ terraform/modules/appservice/outputs.tf
@@ -1,23 +1,23 @@
 output "default_site_hostname" {
   description = "The URL of the deployed App Service"
-  value       = azurerm_app_service.rtrain_app_service.default_site_hostname
+  value       = azurerm_linux_web_app.rtrain_app_service.default_site_hostname
   
 }
 
 output "app_service_principal_id" {
   description = "The principal ID of the App Service"
-  value       = azurerm_app_service.rtrain_app_service.identity[0].principal_id
+  value       = azurerm_linux_web_app.rtrain_app_service.identity[0].principal_id
   
 }
 
 output "app_service_id" {
   description = "value of the App Service ID"
-  value       = azurerm_app_service.rtrain_app_service.id
+  value       = azurerm_linux_web_app.rtrain_app_service.id
   
 }
 
 output "app_service_name" {
   description = "The name of the App Service"
-  value       = azurerm_app_service.rtrain_app_service.name
+  value       = azurerm_linux_web_app.rtrain_app_service.name
   
 }
