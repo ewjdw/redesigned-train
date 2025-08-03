3083d1505d5b822a3d5c57e8d63a10e3a62caa83
wjdw <wpiasecka@gmail.com>
2025-08-03 17:19:58 +0200

    correct app service resource

--- terraform/modules/appservice/main.tf
+++ terraform/modules/appservice/main.tf
@@ -2,30 +2,33 @@ resource "azurerm_service_plan" "rtrain_service_plan" {
   name                = "asp-${var.workload}-${var.environment}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  os_type = "Linux"
-  sku_name = var.sku_name
+  os_type             = "Linux"
+  sku_name            = var.sku_name
   
   tags = var.tags 
   
 }
 
-resource "azurerm_app_service" "rtrain_app_service" {
+resource "azurerm_linux_web_app" "rtrain_app_service" {
   name                = "app-${var.workload}-${var.environment}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_service_plan.rtrain_service_plan.id
-  
+  service_plan_id = azurerm_service_plan.rtrain_service_plan.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+
   site_config {
-    linux_fx_version = "DOCKER|${var.acr_login_server}/rtrainapp:${var.workload}-${var.environment}"
+    application_stack {
+      docker_image_name   = "${var.acr_login_server}/${var.workload}-${var.environment}:latest"
+      docker_registry_url = "https://${var.acr_login_server}"
+    }
   }
 
   app_settings = {
     "ConnectionStrings__DefaultConnection" = "Server=tcp:${var.sql_server_fqdn},1433;Initial Catalog=${var.sql_database_name};Authentication=Active Directory Managed Identity;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
     DOCKER_REGISTRY_SERVER_URL = "https://${var.acr_login_server}"
-  }
-
-  identity {
-    type = "SystemAssigned"
   }
 
   tags = var.tags
