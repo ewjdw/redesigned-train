04fb406d94adafc8c9632a08efdbe014ef5d2d1e
wjdw <wpiasecka@gmail.com>
2025-08-04 01:06:12 +0200

    set auth method to SP

--- scripts/create_or_update_db_user.ps1
+++ scripts/create_or_update_db_user.ps1
@@ -3,16 +3,6 @@ $ErrorActionPreference = "Stop"
 Write-Host "Creating or updating database user..."
 
 try {
-  az login --service-principal `
-    --username $env:AZURE_CLIENT_ID `
-    --password $env:AZURE_CLIENT_SECRET `
-    --tenant $env:AZURE_TENANT_ID `
-    --allow-no-subscriptions
-
-  az account set --subscription $env:AZURE_SUBSCRIPTION_ID
-
-  $accessToken = az account get-access-token --resource https://database.windows.net/ --query accessToken -o tsv
-
   $sqlCommand = @"
 DROP USER IF EXISTS [$($env:APP_PRINCIPAL_ID)];
 CREATE USER [$($env:APP_PRINCIPAL_ID)] FROM EXTERNAL PROVIDER;
@@ -22,9 +12,10 @@ ALTER ROLE db_datawriter ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
 
   sqlcmd -S $env:SQL_FQDN `
     -d $env:SQL_DB `
-    -G `
-    -P $accessToken `
-    -Q $sqlCommand
+    -U $env:AZURE_CLIENT_ID `
+    -P $env:AZURE_CLIENT_SECRET `
+    -Q $sqlCommand `
+    --authentication-method ActiveDirectoryServicePrincipal `
 
 }
 catch {
