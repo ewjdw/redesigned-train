686743a50beeed967cab09ec4b94dc0106bf353c
wjdw <wpiasecka@gmail.com>
2025-08-03 20:22:03 +0200

    run sql commands from windows runner

---
+++ scripts/create_or_update_db_user.ps1
@@ -0,0 +1,24 @@
+$ErrorActionPreference = "Stop"
+
+Write-Host "Creating or updating database user..."
+
+az login --service-principal `
+  --username $env:AZURE_CLIENT_ID `
+  --password $env:AZURE_CLIENT_SECRET `
+  --tenant $env:AZURE_TENANT_ID `
+  --allow-no-subscriptions
+
+az account set --subscription $env:AZURE_SUBSCRIPTION_ID
+
+$sqlCommand = @"
+DROP USER IF EXISTS [$($env:APP_PRINCIPAL_ID)];
+CREATE USER [$($env:APP_PRINCIPAL_ID)] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
+ALTER ROLE db_datawriter ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
+"@
+
+sqlcmd -S $env:SQL_FQDN `
+  -d $env:SQL_DB `
+  -U $env:SQL_ADMIN_LOGIN `
+  -P $env:SQL_ADMIN_PASSWORD `
+  -Q $sqlCommand
