3bf8b169b880a1070d4b97bb152f1ee6bbde7acb
wjdw <wpiasecka@gmail.com>
2025-08-04 09:29:29 +0200

    one-line command

--- scripts/create_or_update_db_user.ps1
+++ scripts/create_or_update_db_user.ps1
@@ -3,15 +3,14 @@ $ErrorActionPreference = "Stop"
 Write-Host "Creating or updating database user..."
 
 try {
-  sqlcmd -S $env:SQL_FQDN `
-    -d $env:SQL_DB `
-    --authentication-method ActiveDirectoryServicePrincipal `
-    -U $env:AZURE_CLIENT_ID `
-    -P $env:AZURE_CLIENT_SECRET `
-    -Q DROP USER IF EXISTS [$($env:APP_PRINCIPAL_ID)]; `
-    -Q CREATE USER [$($env:APP_PRINCIPAL_ID)] FROM EXTERNAL PROVIDER; `
-    -Q ALTER ROLE db_datareader ADD MEMBER [$($env:APP_PRINCIPAL_ID)]; `
-    -Q ALTER ROLE db_datawriter ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
+  $sqlCommand = @"
+DROP USER IF EXISTS [$($env:APP_PRINCIPAL_ID)];
+CREATE USER [$($env:APP_PRINCIPAL_ID)] FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
+ALTER ROLE db_datawriter ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
+"@
+
+  sqlcmd -S $env:SQL_FQDN -d $env:SQL_DB --authentication-method ActiveDirectoryServicePrincipal -U $env:AZURE_CLIENT_ID -P $env:AZURE_CLIENT_SECRET -Q $sqlCommand
 
   if ($LASTEXITCODE -ne 0) {
     Write-Error "Failed to create or update database user."
