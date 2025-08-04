80065c4bd417645688e31ba2a15daea017d96f6a
wjdw <wpiasecka@gmail.com>
2025-08-04 11:35:53 +0200

    invoke-sqlcmd

--- scripts/create_or_update_db_user.ps1
+++ scripts/create_or_update_db_user.ps1
@@ -10,19 +10,9 @@ ALTER ROLE db_datareader ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
 ALTER ROLE db_datawriter ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
 "@
 
-  $sqlcmdArguments = @(
-    "-S", "$($env:SQL_FQDN)",
-    "-d", "$($env:SQL_DB)",
-    "--authentication-method", "ActiveDirectoryServicePrincipal",
-    "-U", "$($env:AZURE_CLIENT_ID)",
-    "-P", "$($env:AZURE_CLIENT_SECRET)",
-    "-Q", $sqlCommand
-  )
-  Write-host "Executing SQL command with args:"
-  for ($i = 0; $i -lt $sqlcmdArguments.Count; $i++) {
-    Write-Host "  [$i]: $($sqlcmdArguments[$i])"
-  }
-  sqlcmd @sqlcmdArguments
+  az login --service-principal --username $env:AZURE_CLIENT_ID --password $env:AZURE_CLIENT_SECRET --tenant $env:AZURE_TENANT_ID
+  $accessToken = az account get-access-token --resource https://database.windows.net/ --query accessToken -o tsv
+  Invoke-Sqlcmd -Query $sqlCommand -ServerInstance $env:SQL_FQDN -Database $env:SQL_DB -AccessToken $accessToken
 
   if ($LASTEXITCODE -ne 0) {
     Write-Error "Failed to create or update database user."
