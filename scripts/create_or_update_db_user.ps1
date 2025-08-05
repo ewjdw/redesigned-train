54603a80d120ba27ee75126713bc11a67382f8d9
wjdw <wpiasecka@gmail.com>
2025-08-05 15:06:26 +0200

    test connection query

--- scripts/create_or_update_db_user.ps1
+++ scripts/create_or_update_db_user.ps1
@@ -3,15 +3,18 @@ $ErrorActionPreference = "Stop"
 Write-Host "Creating or updating database user..."
 
 try {
+  # az login --service-principal --username $env:AZURE_CLIENT_ID --password $env:AZURE_CLIENT_SECRET --tenant $env:AZURE_TENANT_ID
+  $accessToken = az account get-access-token --resource https://database.windows.net/ --query accessToken -o tsv
+
+  $connTest = "SELECT SYSTEM_USER as CurrentUser, USER_NAME() as DatabaseUser, ORIGINAL_LOGIN() as OriginalLogin;"
+  Invoke-Sqlcmd -Query $connTest -ServerInstance $env:SQL_FQDN -Database $env:SQL_DB -AccessToken $accessToken
+
   $sqlCommand = @"
 DROP USER IF EXISTS [$($env:APP_PRINCIPAL_ID)];
 CREATE USER [$($env:APP_PRINCIPAL_ID)] FROM EXTERNAL PROVIDER;
 ALTER ROLE db_datareader ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
 ALTER ROLE db_datawriter ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
 "@
-
-  # az login --service-principal --username $env:AZURE_CLIENT_ID --password $env:AZURE_CLIENT_SECRET --tenant $env:AZURE_TENANT_ID
-  $accessToken = az account get-access-token --resource https://database.windows.net/ --query accessToken -o tsv
   
   try {
     Invoke-Sqlcmd -Query $sqlCommand -ServerInstance $env:SQL_FQDN -Database $env:SQL_DB -AccessToken $accessToken
