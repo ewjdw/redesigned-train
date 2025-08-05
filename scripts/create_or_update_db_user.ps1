278f8a52b75b49b5b5a154c186730ae7aae79bf8
wjdw <wpiasecka@gmail.com>
2025-08-05 14:38:13 +0200

    sqlcmd

--- scripts/create_or_update_db_user.ps1
+++ scripts/create_or_update_db_user.ps1
@@ -10,11 +10,12 @@ ALTER ROLE db_datareader ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
 ALTER ROLE db_datawriter ADD MEMBER [$($env:APP_PRINCIPAL_ID)];
 "@
 
-  az login --service-principal --username $env:AZURE_CLIENT_ID --password $env:AZURE_CLIENT_SECRET --tenant $env:AZURE_TENANT_ID
-  $accessToken = az account get-access-token --resource https://database.windows.net/ --query accessToken -o tsv
+  # az login --service-principal --username $env:AZURE_CLIENT_ID --password $env:AZURE_CLIENT_SECRET --tenant $env:AZURE_TENANT_ID
+  # $accessToken = az account get-access-token --resource https://database.windows.net/ --query accessToken -o tsv
   
   try {
-    Invoke-Sqlcmd -Query $sqlCommand -ServerInstance $env:SQL_FQDN -Database $env:SQL_DB -AccessToken $accessToken
+    # Invoke-Sqlcmd -Query $sqlCommand -ServerInstance $env:SQL_FQDN -Database $env:SQL_DB -AccessToken $accessToken
+    sqlcmd -S $env:SQL_FQDN -d $env:SQL_DB --authenticationMethod ActiveDirectoryServicePrincipal -U $env:APP_PRINCIPAL_ID -P $env:APP_PRINCIPAL_ID -Q $sqlCommand
   }
   catch {
     Write-Error "invoke-sqlcmd failed: $($_.Exception.Message)"
