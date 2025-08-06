39e3c9ecbdffe2bd70ca4649517af097afe4d9c4
wjdw <wpiasecka@gmail.com>
2025-08-06 09:21:13 +0200

    cleanup [no ci]

--- scripts/create_or_update_db_user.ps1
+++ scripts/create_or_update_db_user.ps1
@@ -3,15 +3,7 @@ $ErrorActionPreference = "Stop"
 Write-Host "Creating or updating database user..."
 
 try {
-  # az login --service-principal --username $env:AZURE_CLIENT_ID --password $env:AZURE_CLIENT_SECRET --tenant $env:AZURE_TENANT_ID
   $accessToken = az account get-access-token --resource https://database.windows.net/ --query accessToken -o tsv
-
-  # $connTest = "SELECT SYSTEM_USER as CurrentUser, USER_NAME() as DatabaseUser, ORIGINAL_LOGIN() as OriginalLogin;"
-  # $connRsult = Invoke-Sqlcmd -Query $connTest -ServerInstance $env:SQL_FQDN -Database $env:SQL_DB -AccessToken $accessToken
-
-  # Write-Host "Current User: $($connRsult.CurrentUser)"
-  # Write-Host "Database User: $($connRsult.DatabaseUser)"
-  # Write-Host "Original Login: $($connRsult.OriginalLogin)"
 
   $sqlCommand = @"
 DROP USER IF EXISTS [$($env:APP_SERVICE_NAME)];
@@ -22,7 +14,7 @@ ALTER ROLE db_datawriter ADD MEMBER [$($env:APP_SERVICE_NAME)];
   
   try {
     Invoke-Sqlcmd -Query $sqlCommand -ServerInstance $env:SQL_FQDN -Database $env:SQL_DB -AccessToken $accessToken
-    # sqlcmd -S $env:SQL_FQDN -d $env:SQL_DB --authenticationMethod ActiveDirectoryServicePrincipal -U $env:APP_PRINCIPAL_ID -P $env:APP_PRINCIPAL_ID -Q $sqlCommand
+    Write-Host "Database user [$($env:APP_SERVICE_NAME)] created or updated successfully."
   }
   catch {
     Write-Error "invoke-sqlcmd failed: $($_.Exception.Message)"
